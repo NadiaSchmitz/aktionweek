e05a1571787765ab691079fa5d392244f61ed714
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-02-22 11:01:14 +0100

    22022022

--- src/Main.java
+++ src/Main.java
@@ -9,42 +9,49 @@ public class Main {
 		umsatz_bisher = IOTools.readInteger("Geben Sie Ihren bisherigen Umsatz ein: ");
 		rabatt = 0;
 		
-		if (umsatz_aktuell <= 100 && umsatz_bisher <= 500) {
-			
-		} else if (umsatz_aktuell <= 100 && umsatz_bisher > 500) {
-			rabatt = 2;
-		} else if (umsatz_aktuell > 100 && umsatz_aktuell <= 1000 && umsatz_bisher >= 500 && umsatz_bisher <= 1000) {
-			rabatt = 3;
-		} else if (umsatz_aktuell > 100 && umsatz_aktuell <= 1000 && umsatz_bisher > 1000) {
-			rabatt = 4;
-		} else if (umsatz_aktuell > 1000 && umsatz_aktuell <= 5000 && umsatz_bisher > 1000 && umsatz_bisher <= 2500) {
-			rabatt = 5;
-		} else if (umsatz_aktuell > 1000 && umsatz_aktuell <= 5000 && umsatz_bisher > 2500) {
-			rabatt = 6;
-		} else if (umsatz_aktuell > 5000) {
-			rabatt = 7;
-		}
-		
-// Funktioniert nicht
-		
 //		if (umsatz_aktuell <= 100 && umsatz_bisher <= 500) {
 //			
 //		} else if (umsatz_aktuell <= 100 && umsatz_bisher > 500) {
 //			rabatt = 2;
-//		} else if (umsatz_aktuell <= 1000 && umsatz_bisher <= 1000) {
+//		} else if (umsatz_aktuell > 100 && umsatz_aktuell <= 1000 && umsatz_bisher >= 500 && umsatz_bisher <= 1000) {
 //			rabatt = 3;
-//		} else if (umsatz_aktuell <= 1000 && umsatz_bisher > 1000) {
+//		} else if (umsatz_aktuell > 100 && umsatz_aktuell <= 1000 && umsatz_bisher > 1000) {
 //			rabatt = 4;
-//		} else if (umsatz_aktuell <= 5000 && umsatz_bisher <= 2500) {
+//		} else if (umsatz_aktuell > 1000 && umsatz_aktuell <= 5000 && umsatz_bisher > 1000 && umsatz_bisher <= 2500) {
 //			rabatt = 5;
-//		} else if (umsatz_aktuell <= 5000 && umsatz_bisher > 2500) {
+//		} else if (umsatz_aktuell > 1000 && umsatz_aktuell <= 5000 && umsatz_bisher > 2500) {
 //			rabatt = 6;
 //		} else if (umsatz_aktuell > 5000) {
 //			rabatt = 7;
 //		}
 		
-		System.out.println("Ihr Rabatt beträgt: " + rabatt);
 		
+		if (umsatz_bisher <= 500) {
+			System.out.println("Bedingung 0");
+			rabatt = 0;
+		} else if (umsatz_aktuell <= 100 & umsatz_bisher > 500) {
+			System.out.println("Bedingung 1");
+			rabatt = 2;
+		} else if (umsatz_aktuell <= 1000 & umsatz_bisher <= 1000) {
+			System.out.println("Bedingung 2");
+			rabatt = 3;
+		} else if (umsatz_aktuell <= 1000 & umsatz_bisher > 1000) {
+			System.out.println("Bedingung 3");
+			rabatt = 4;
+		} else if (umsatz_aktuell <= 5000 & umsatz_bisher <= 2500) {
+			System.out.println("Bedingung 4");
+			rabatt = 5;
+		} else if (umsatz_aktuell <= 5000 & umsatz_bisher > 2500) {
+			System.out.println("Bedingung 5");
+			rabatt = 6;
+		} 
+		
+		if (umsatz_aktuell > 5000) {
+			System.out.println("Bedingung 6");
+			rabatt = 7;
+		}
+		
+		System.out.println("Ihr Rabatt beträgt: " + rabatt + "%");
 	}
 
 }
